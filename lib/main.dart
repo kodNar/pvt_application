84ca78adff78ae722aee548fb1c54dedb63b19ba
kodNar <56548619+kodNar@users.noreply.github.com>
2020-03-31 14:31:09 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Einars app'),
+      home: MyHomePage(title: 'PVT App'),
     );
   }
 }
