abc007ebc62283631711577c2815a39d75298ec3
kodNar <56548619+kodNar@users.noreply.github.com>
2020-04-07 12:17:09 +0200

    Testnar Testar

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => new _LoginPageState();
 
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email, _password;
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login here"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input){
+                if(input.isEmpty){ //Check if auth sign or something
+                return 'Please type an Email';
+                }
+                return 'Hej';
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(
+                labelText: 'Email'
+              ),
+            ),
+            TextFormField(
+              validator: (input){
+                if(input.isEmpty){
+                  return 'Please type password';
+                }
+                return 'Welcome';
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
