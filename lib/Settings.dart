097ef482b7004571fcd215d713a84dbfe8b372a1
jacob <Jacoblinden@hotmail.com>
2020-04-07 10:19:12 +0200

    Setting title

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/HomePage.dart';
 
 class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+          title: Text('Settings'),
+          actions: <Widget>[
+          ]
+      ),
+
       floatingActionButton: FloatingActionButton(
 
         onPressed: () {
           Navigator.pop(context);
         },
+
         child: Icon(Icons.save)
       ),
       body: Container(
