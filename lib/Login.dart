82c41aae3c069f8ad28e509f7c428bd832754fdf
kodNar <56548619+kodNar@users.noreply.github.com>
2020-04-07 10:41:29 +0200

    Testnar Testar

--- lib/Login.dart
+++ lib/Login.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
    @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login"),
+      appBar: AppBar(title: Text("Login here"),
       ),
     );
   }
