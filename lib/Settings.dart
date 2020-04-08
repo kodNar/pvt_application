bb72f79099f3ac450e71c7d26957d97836bbe85c
jacob <Jacoblinden@hotmail.com>
2020-04-08 12:21:45 +0200

    Setting title

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -5,46 +5,41 @@ import 'LabeledSwitch.dart';
 class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-          title: Text('Settings'),
-          actions: <Widget>[
-          ]
-      ),
-      body:
+        appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text('Settings'),
+            actions: <Widget>[]),
+        body: Builder(builder: (context) {
+          return Stack(
+            children: [
+              Container(
+                child:
+                ListView(
+                  children: <Widget>[
+                    Container(
+                      child: MyStatefulWidget(),
+                    ),
+                    Container(
+                      child: MyTextInput(),
+                      height: 60,
+                    ),
+                    Container(
+                      child: MyStatefulWidget2(),
+                    ),
+                  ],
+                ),
 
-      ListView(
-        children: <Widget>[
-          Container(
-
-            child:
-             MyStatefulWidget(),
-          ),
-          Container(
-             child:
-              MyTextInput(),
-              height: 60,
               ),
-
-          Container(
-
-            child:
-            MyStatefulWidget2(),
-          ),
-
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-
-
-        child: Icon(Icons.save)
-      ),
-    );
+              Container(
+                alignment: Alignment.bottomRight,
+                  padding:EdgeInsets.all(20.0),
+                  child:
+                  SnackBarPage()
+              ),
+              ]
+          );
+        }));
   }
 }
 
@@ -61,23 +56,52 @@ class _MyTextInput extends State<MyTextInput> {
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
   final myController = TextEditingController();
+
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
     myController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return
-       new TextField(
-          controller: myController,
-         onChanged: (String str){
-            setState(() {
-              //save the input
-            });
-         }
-      );
+    return new TextField(
+        controller: myController,
+        onChanged: (String str) {
+          setState(() {
+            //save the input
+          });
+        });
   }
 }
+
 //////////////////////////////////////////////////////////////////
+////////////////////snackbar code///////////////////////////////////////////
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FloatingActionButton.extended(
+        icon: Icon(Icons.save),
+        label: Text("Save"),
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Saved!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+
+          );
+
+          Scaffold.of(context).showSnackBar(snackBar);
+          Icon(Icons.check);
+        },
+      ),
+    );
+  }
+}
+///////////////////////////////////////////////////////////////////////
