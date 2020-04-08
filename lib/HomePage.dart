6965447c5f0a351fbe5b7098f77eccb7a538c242
kodNar <56548619+kodNar@users.noreply.github.com>
2020-04-08 13:16:22 +0200

    Snyggat till knapparna, färg, runda, border size.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,10 +7,9 @@ import 'Settings.dart';
 import 'workerData.dart';
 
 class HomePage extends StatefulWidget {
-
-
   HomePage({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -24,80 +23,96 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 132, 50, 155),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+          backgroundColor: Color.fromARGB(255, 132, 50, 155),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.settings),
               tooltip: 'Settings',
               onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => Settings()
-                ));
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) => Settings()));
               },
             ),
-          ]
-      ),
-
-
-
-      body:
-      Container(
+          ]),
+      body: Container(
+        padding: EdgeInsets.all(20),
         width: size.width,
         height: size.height,
         alignment: Alignment.bottomCenter,
         decoration: new BoxDecoration(
-          color: Color.fromARGB(255,132, 50, 155),
+          color: Color.fromARGB(255, 132, 50, 155),
           image: DecorationImage(
             image: AssetImage('assets/images/bakgrund.png'),
             fit: BoxFit.fill,
           ),
-
         ),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            RaisedButton(
-              color: Colors.transparent,
-              onPressed: (){
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => LoginPage()));
-              },
-              child: const Text('Sign in'),
-              textColor: Colors.white,
+            ButtonTheme(
+              minWidth: 150,
+              height: 100,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100.0),
+                  side: BorderSide(color: Colors.green, width: 2.5),
+
+
+                ),
+                color: Colors.transparent,
+                onPressed: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+                child: new Text(
+                  'Sign in',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
             SizedBox(width: 10),
-            RaisedButton(
-              color: Colors.transparent,
-              onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => LoginPage() ));
+            ButtonTheme(
+              minWidth: 150,
+              height: 100,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                  side: BorderSide(color: Colors.green, width: 2.5),
+                ),
+                color: Colors.transparent,
+                onPressed: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => LoginPage()));
                 },
-              child: const Text('Guest'),
-              textColor: Colors.white,
-
-            )
+                child: new Text(
+                  'Guest',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
-
-
-
-
-     bottomNavigationBar: BottomAppBar(
-       color:Color.fromARGB(255, 132, 50, 155),
-       shape: CircularNotchedRectangle(),
-       elevation: 0.0,
-       child: new Row(
-         mainAxisSize: MainAxisSize.max,
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: <Widget>[
-           Text("Stockholm Stad"),
-
-           Padding(
-             padding: const EdgeInsets.all(55.0),
+      bottomNavigationBar: BottomAppBar(
+        color: Color.fromARGB(255, 132, 50, 155),
+        shape: CircularNotchedRectangle(),
+        elevation: 0.0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Stockholm Stad"),
+            Padding(
+              padding: const EdgeInsets.all(55.0),
             ),
           ],
         ),
