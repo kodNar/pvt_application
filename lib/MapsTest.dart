cfecc8c8ec9095221c39b7b511da5f4000ac2376
Simon <simon.schoolsoft@gmail.com>
2020-04-16 09:51:31 +0200

    Ny API key till maps

--- lib/MapsTest.dart
+++ lib/MapsTest.dart
@@ -23,13 +23,13 @@ class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(59.328560, 18.065836),
     zoom: 14.4746,
   );
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(59.328560, 18.065836),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
@@ -37,7 +37,7 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
