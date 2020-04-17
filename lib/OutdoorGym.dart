50d605887a84d43bc11dc73d9f8b2992795aee24
jacob <Jacoblinden@hotmail.com>
2020-04-17 09:26:35 +0200

    Setting title

--- lib/OutdoorGym.dart
+++ lib/OutdoorGym.dart
@@ -1,13 +1,24 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 class OutdoorGym {
   String name;
-  double nycLat;
-  double nycLng;
-  Marker marker;
+  double lat;
+  double lng;
   //later be equipmmnt insted of string
   List<String> equipments= [];
+  Marker marker;
   //home/office/unknown
 
-  OutdoorGym({this.name, this.nycLat, this.nycLng});
+  OutdoorGym(String name,String lat,String lng){
+   this.name= name;
+   this.lat =00;
+   this.lng = 00;
+   this.marker = new Marker(
+     markerId: MarkerId (name),
+     position: LatLng (this.lat,this.lng),
+       onTap:(){
+       //smart shiiit
+   },
+   draggable: false
+   );
+  }
 }
