7bef97f50a6caaf09d3f9e1fd6ab03c5a942c24f
jacob <Jacoblinden@hotmail.com>
2020-04-20 09:56:36 +0200

    Setting title

--- lib/GenericGymPage.dart
+++ lib/GenericGymPage.dart
@@ -38,10 +38,24 @@ class GenericGymPage extends StatelessWidget {
                           color: Colors.transparent,
                           width: MediaQuery.of(context).size.width,
                           height: 60,
-                          child: buttonR(name: "Equipment", icon:Icon(
-                            Icons.airline_seat_legroom_extra,
-                            color: Colors.white,
-                          ))
+                          child: RaisedButton.icon(
+                            icon: Icon(
+                              Icons.airline_seat_legroom_extra,
+                              color: Colors.white,
+                            ),
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(30.0),
+                            ),
+                            onPressed: () {},
+                            color: Color.fromARGB(255, 132, 50, 155),
+                            label: Text(
+                              "Equipments",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                              ),
+                            ),
+                          ),
                         ),
                         Container(
                           padding: const EdgeInsets.all(10.0),
@@ -103,41 +117,6 @@ class GenericGymPage extends StatelessWidget {
             ),
     );
   }
-}class buttonR extends StatefulWidget {
-  String name;
-  Icon icon;
-  buttonR({this.name, this.icon});
-
-  @override
-  _buttonR createState() => _buttonR(icon, name);
 }
 
-class _buttonR extends State<buttonR> {
-  Icon icon;
-  String name;
-  _buttonR(Icon icon, String name){
-    this.icon =icon;
-    this.name = name;
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton.icon(
-      icon: (icon),
-      shape: new RoundedRectangleBorder(
-        borderRadius: new BorderRadius.circular(30.0),
-      ),
-      onPressed: () {},
-      color: Color.fromARGB(255, 132, 50, 155),
-      label: Text(
-        name,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 22.0,
-        ),
-      ),
-    );
-  }
-}
 
