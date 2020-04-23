9e56c40ada18ce5ed916b469fb24aa31a2c6aed3
kodNar <56548619+kodNar@users.noreply.github.com>
2020-04-23 12:25:48 +0200

    Snyggade till Login

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -18,7 +18,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login here"),
+      appBar: AppBar(
+        title: Text("Login here"),
       ),
       body: Form(
         key: _formKey,
@@ -26,50 +27,97 @@ class _LoginPageState extends State<LoginPage> {
           color: Color.fromARGB(255, 132, 50, 155),
           child: Column(
             children: <Widget>[
-              TextFormField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                cursorColor: Colors.white,
-                // ignore: missing_return
-                validator: (input){
-                  if(input.isEmpty){ //Check if auth sign or something
-                    return 'Please provide an Email';
-                  }
-                },
-                onSaved: (input) => _email = input,
-                decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    )
+              Container(
+                margin: EdgeInsets.all(10.0),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Stockholm_endast_logga_vit.png'),
+                  ),
                 ),
               ),
-              TextFormField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                cursorColor: Colors.white,
-                // ignore: missing_return
-                validator: (input){
-                  if(input.isEmpty){
-                    return 'Please provide a password';
-                  }
-                  if(input.length < 6){
-                    return 'Your password must be atleast 6 characters';
-                  }
-                },
-                onSaved: (input) => _password = input,
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
+              Text("AppName",
+                  style: TextStyle(
                       color: Colors.white,
-                    )
+                      fontFamily: 'Stockholm',
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30)),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/applogga_vit_liten.png'),
+                  ),
                 ),
-
-                obscureText: true, //Döljer texten
               ),
-
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                    hintText: "Email",
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  cursorColor: Colors.white,
+                  // ignore: missing_return
+                  validator: (input) {
+                    if (input.isEmpty) {
+                      //Check if auth sign or something
+                      return 'Please provide an Email';
+                    }
+                  },
+                  onSaved: (input) => _email = input,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: "Password",
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  cursorColor: Colors.white,
+                  // ignore: missing_return
+                  validator: (input) {
+                    if (input.isEmpty) {
+                      return 'Please provide a password';
+                    }
+                    if (input.length < 6) {
+                      return 'Your password must be atleast 6 characters';
+                    }
+                  },
+                  onSaved: (input) => _password = input,
+                  obscureText: true, //Döljer texten
+                ),
+              ),
               Container(
                 padding: EdgeInsets.only(top: 20),
                 child: RaisedButton(
@@ -80,62 +128,55 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 padding: EdgeInsets.only(top: 20),
                 child: GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     final formState = _formKey.currentState;
                     formState.save();
-                    if(_email.isEmpty) {
-                      showDialog(context: context,
-                      builder: (BuildContext context)  {
-                        return AlertDialog(
-                          title: Text(
-                            "Failed reset"
-                          ),
-                          content: Text(
-                            "Please input your e-mail in the e-mail field"
-                          ),
-                          actions: <Widget>[
-                            new FlatButton(
-                              child: Text(
-                                "Ok"
-                              ),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            )
-                          ],
-                        );
-                      }
-                      );
+                    if (_email.isEmpty) {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Failed reset"),
+                              content: Text(
+                                  "Please input your e-mail in the e-mail field"),
+                              actions: <Widget>[
+                                new FlatButton(
+                                  child: Text("Ok"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                )
+                              ],
+                            );
+                          });
                     }
                     // ignore: unnecessary_statements
-                    else{
-                        sendPasswordResetEmail(_email);
-                        showDialog(context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text(
-                                "Reset password"
-                            ),
-                            content: Text(
-                                "An e-mail to reset your password has been sent to your registered e-mail."
-                            ),
-                            actions: <Widget>[
-                              new FlatButton(
-                                child: Text(
-                                  "Ok",
-                                ),
-                                onPressed: ()
-                                {
-                                  Navigator.of(context).pop();
+                    else {
+                      sendPasswordResetEmail(_email);
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Reset password"),
+                              content: Text(
+                                  "An e-mail to reset your password has been sent to your registered e-mail."),
+                              actions: <Widget>[
+                                new FlatButton(
+                                  child: Text(
+                                    "Ok",
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
                                   },
-                              )
-                            ],
-                          );
-                        }
-                        );
-                    };
-                    },
-                  child: Text('Forgot password? Click here',
+                                )
+                              ],
+                            );
+                          });
+                    }
+                    ;
+                  },
+                  child: Text(
+                    'Forgot password? Click here',
                     style: TextStyle(
                       fontSize: 15,
                       fontWeight: FontWeight.bold,
@@ -147,10 +188,12 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 padding: EdgeInsets.only(top: 20),
                 child: GestureDetector(
-                  onTap: (){
-                    Navigator.push(context,MaterialPageRoute(builder:(context) => Register() ));
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Register()));
                   },
-                  child: Text('No account? Click here',
+                  child: Text(
+                    'No account? Click here',
                     style: TextStyle(
                       fontSize: 15,
                       fontWeight: FontWeight.bold,
@@ -166,23 +209,25 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-
   //reset password
   Future sendPasswordResetEmail(String email) async {
     return _firebaseAuth.sendPasswordResetEmail(email: email);
   }
 
   Future<void> signIn() async {
-
     final formState = _formKey.currentState;
 
-    if (formState.validate()) { //Checks so that the inputs are correct
+    if (formState.validate()) {
+      //Checks so that the inputs are correct
       formState.save(); //ser till att vi kan hämta variablerna.
 
       try {
-        AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password); //Confirming the e-mail and password towards the firebase database
-        Navigator.push(context, MaterialPageRoute(builder: (context) => MapSample()));
-
+        AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: _email,
+            password:
+                _password); //Confirming the e-mail and password towards the firebase database
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MapSample()));
       } catch (e) {
         print(e.message);
       }
