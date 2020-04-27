76b3ea3b353c4e5dbd4da3dd30cf62be25d4a56a
jacob <Jacoblinden@hotmail.com>
2020-04-27 13:58:07 +0200

    loading text file and adding them to map

--- lib/pages/TestPage.dart
+++ lib/pages/TestPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/services/DatabaseTestPage.dart';
 
+import 'JacobTest.dart';
+
 class TestPage extends StatefulWidget {
   @override
   _TestPageState createState() => _TestPageState();
@@ -91,7 +93,7 @@ class _TestPageState extends State<TestPage> {
               padding: EdgeInsets.fromLTRB(0,10,0,10),
               child: GestureDetector(
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => DatabaseTestPage() )); /** Sätt din testsida här! **/
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MapSampleJacobo() )); /** Sätt din testsida här! **/
                 },
                 child: Text(
                   'Jacobs testknapp',
