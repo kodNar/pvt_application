44ee6e47146a3a5101d88e6f4f06c9a702a48180
jacob <Jacoblinden@hotmail.com>
2020-05-06 10:20:10 +0200

    fixed error moving to the wrong location

--- lib/pages/ExistingWorkouts.dart
+++ lib/pages/ExistingWorkouts.dart
@@ -29,35 +29,35 @@ class _ExistingState extends State<ExistingWorkouts> {
                               child: ListView.builder(
                                   itemCount: snapshot.data.length,
                                   itemBuilder: (context, index) {
-                                    return Container(
+                                    return RaisedButton(
                                         color: Color.fromARGB(
-                                            255, 132 + index * 30, 50, 155),
-                                        height: 50,
+                                            255, 200+ index * 30, 50, 155),
                                         padding: EdgeInsets.all(10),
                                         child: Row(
                                             mainAxisAlignment:
                                                 MainAxisAlignment.spaceBetween,
                                             children: <Widget>[
-                                              Flexible(
-                                                fit: FlexFit.tight,
-                                                flex: 5,
-                                                child: InkWell(
-                                                  onTap: () {},
-                                                  child: RichText(
-                                                    overflow:
-                                                        TextOverflow.ellipsis,
-                                                    strutStyle: StrutStyle(
-                                                        fontSize: 16.0),
-                                                    text: TextSpan(
-                                                        style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontWeight:
-                                                                FontWeight
-                                                                    .bold),
-                                                        text: snapshot.data[index]),
+                                              Container(child: InkWell(
+                                                splashColor: Colors.blue,
+                                                highlightColor: Colors.purpleAccent,
+                                                onTap: () {
+                                                },
+                                                child: RichText(
+                                                  overflow:
+                                                  TextOverflow.ellipsis,
+                                                  strutStyle: StrutStyle(
+                                                      fontSize: 10.0),
+                                                  text: TextSpan(
+                                                      style: TextStyle(
+                                                          fontSize: 26,
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                          FontWeight
+                                                              .bold),
+                                                      text: snapshot.data[index]
                                                   ),
                                                 ),
-                                              ),
+                                              ),)
                                             ]));
                                   }))
                           : Center();
@@ -67,10 +67,6 @@ class _ExistingState extends State<ExistingWorkouts> {
   Future<List<String>> _getSessions() async {
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
       List<String> workouts =await DatabaseService(uid: user.uid).getWorkouts();
-      print(workouts.length);
-      workouts.forEach((e)=>
-          print(e+"fewewfewfwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-      );
       return workouts;
   }
     }
