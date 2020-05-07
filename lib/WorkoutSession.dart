4b6ef02ebd72e43652347a149404c7c36652aa75
jacob <Jacoblinden@hotmail.com>
2020-05-07 10:24:34 +0200

    fixed error moving to the wrong location

--- lib/WorkoutSession.dart
+++ lib/WorkoutSession.dart
@@ -3,17 +3,22 @@ import 'package:flutterapp/User.dart';
 import 'package:flutterapp/Exercise.dart';
 
 class WorkoutSession{
-
+  String _name;
   User _user;
   OutdoorGym _location;
   DateTime _dateTime;
 
   List <Exercise> _exercises;
 
-  WorkoutSession(User user, OutdoorGym location){
+  WorkoutSession(String name,User user, OutdoorGym location,DateTime dateTime){
     _user = user;
     _location = location;
+    _name = name;
+    _dateTime = dateTime;
+
   }
+
+  String get name => _name;
 
   User getUser(){
     return _user;
