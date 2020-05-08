9e806e15331fc5f36d280bb07040280c45df4524
kodNar <56548619+kodNar@users.noreply.github.com>
2020-05-08 12:30:15 +0200

    Log workout select gym works. v2

--- lib/pages/WorkoutLog.dart
+++ lib/pages/WorkoutLog.dart
@@ -69,21 +69,6 @@ class _WorkoutLogState extends State<WorkoutLog> {
     );
   }
 
-  void _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => WorkoutGymList()),
-    );
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text("$result")));
-  }
-
   Widget gymReturn(){
     if(gymChosen){
       return Container(
