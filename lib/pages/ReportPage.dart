7b3725704eaaf97eecda0bdb0c1e09d9fd983815
Simon <simon.schoolsoft@gmail.com>
2020-05-11 13:01:02 +0200

    Kommer åt kamera

--- lib/pages/ReportPage.dart
+++ lib/pages/ReportPage.dart
@@ -1,58 +1,368 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/Equipment.dart';
 import 'package:flutterapp/FaultyEquipmentReport.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
+import 'package:flutterapp/pages/MapsTest.dart';
+import 'package:flutterapp/OutdoorGym.dart';
+import 'package:flutterapp/pages/AboutUs.dart';
+import 'package:camera/camera.dart';
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
 
 
 class ReportPage extends StatelessWidget {
+  final List<OutdoorGym> allOutdoorGym = MapSampleState.allOutdoorGym;
+  List<DropdownMenuItem<OutdoorGym>> dropDownMenuItems;
+  OutdoorGym selectedGym;
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+  bool isCameraReady = false;
+  bool showCapturedPhoto = false;
+//  bool mounted = false;
+  var ImagePath;
+
+
+
+
+
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Report an issue',
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text(
-              'Epost',
-            ),
-            onPressed: main,
-          ),
-        ],
-
-      ),
-    );
-  }
-
-
-
-  main() async {
-    String userName = 'apikey';
-    String passWord = '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';
-    final smtpServer = SmtpServer('smtp.sendgrid.net', username: userName, password: passWord);
-
-    final message = Message()
-      ..from = Address('simon.schoolsoft@gmail.com', 'Simon')
-      ..recipients.add('simon.schoolsoft@gmail.com')
-      ..subject = 'Test Dart Mailer library ${DateTime.now()}'
-      ..text = 'This is the plain text.\nThis is line 2 of the text part.';
-
-
-    try {
-      final sendReport = await send(message, smtpServer);
-      print('Message sent: ' + sendReport.toString());
-    } on MailerException catch (e) {
-      print('Message not sent.');
-      for (var p in e.problems) {
-        print('Problem: ${p.code}: ${p.msg}');
-      }
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _controller != null
+          ? _initializeControllerFuture = _controller.initialize()
+          : null; //on pause camera is disposed, so we need to call again "issue is only for android"
     }
   }
 
+
+
+  List<DropdownMenuItem<OutdoorGym>> buildDropDownMenuItems(List gyms){
+    List<DropdownMenuItem<OutdoorGym>> items = List();
+    for(OutdoorGym gym in allOutdoorGym){
+      items.add(
+        DropdownMenuItem(
+          value: gym,
+          child: Text(gym.name),
+        ),
+      );
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 132, 50, 155),
+      appBar: AppBar(title: const Text('Report Page'),
+
+
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+
+            ),
+            Container(
+                child: Row(
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: (){
+                        main2();
+                      },
+                    ),
+                    Container(
+
+                    )
+                  ],
+                )
+
+            ),
+            Container(
+              child: Text('Open Comments', style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              ),
+            ),
+            Container(
+
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  prefixIcon: Icon(Icons.description),
+                  hintText: "Describe your issue",
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                cursorColor: Colors.black38,
+                // ignore: missing_return
+//              validator: (input) {
+//                if (input.isEmpty) {
+//                  return 'Please provide a short description';
+//                }
+//              },
+
+              ),
+            ),
+            Container(
+
+              child: Container(
+                alignment: Alignment.center,
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  gradient: LinearGradient(
+                    begin: Alignment.center,
+                    colors: <Color>[
+                      Color(0xFFF57C00),
+                      Color(0xFFFF9800),
+                      Color(0xFFFFA726),
+                    ],
+                  ),
+                ),
+                child: Text('Send',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                    )),
+              ),
+            )
+          ]
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'One';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+
+Future<void> main2() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // Wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner
+      // until the controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the
+            // pattern package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
+
+
+
+
+
+
+
+main() async {
+  String userName = 'apikey';
+  String passWord = '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';
+  final smtpServer = SmtpServer('smtp.sendgrid.net', username: userName, password: passWord);
+
+  final message = Message()
+    ..from = Address('simon.schoolsoft@gmail.com', 'Simon')
+    ..recipients.add('simon.schoolsoft@gmail.com')
+    ..subject = 'Test Dart Mailer library ${DateTime.now()}'
+    ..text = 'This is the plain text.\nThis is line 2 of the text part.';
+
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('Message sent: ' + sendReport.toString());
+  } on MailerException catch (e) {
+    print('Message not sent.');
+    for (var p in e.problems) {
+      print('Problem: ${p.code}: ${p.msg}');
+    }
+  }
 }
