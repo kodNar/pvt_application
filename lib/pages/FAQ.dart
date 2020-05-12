d1f657178d1c0435bb766773304a0e72cf8b9b92
hellstroems <hellstrom.m.johan@gmail.com>
2020-05-12 12:59:42 +0200

    JOHAN HAS ENTERED THE BUILDING

--- lib/pages/FAQ.dart
+++ lib/pages/FAQ.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/widgets/Appbar.dart';
 
 import 'HomePage.dart';
 
@@ -9,6 +10,7 @@ import 'HomePage.dart';
 class FAQ extends StatelessWidget {
   String clickedFAQ;
   String clickedAnswer;
+  String answer1 = "";
 
   createAlertDialog(BuildContext context) {
     return showDialog(context: context, builder: (context) {
@@ -22,157 +24,194 @@ class FAQ extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Colors.black,
       backgroundColor: Color.fromARGB(255, 132, 50, 155),
-      appBar: AppBar(
-        leading: BackButton(
-        ),
-        title: Text("Frequently Asked Questions"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.home,
-            ),
-            tooltip: "Go to homepage",
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-            },
-          )
-        ],
+      appBar: BaseAppBar(
+        title: "Frequently Asked Questions",
       ),
+//      appBar: AppBar(
+//        leading: BackButton(
+//        ),
+//        title: Text("Frequently Asked Questions"),
+//        actions: <Widget>[
+//          IconButton(
+//            icon: Icon(
+//              Icons.home,
+//            ),
+//            tooltip: "Go to homepage",
+//            onPressed: (){
+//              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+//            },
+//          )
+//        ],
+//      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        //crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             padding: EdgeInsets.all(20.0),
-            //color: Colors.cyan,
             alignment: Alignment.center,
             child: Text(
-
                "FAQ",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26.0, color: Colors.white),
             ),
 
           ),
-          Container(
-            height: 400.0,
-            child: ListView(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: (){
-                        clickedFAQ = "Why are some gyms made from wood and some from steel?";
-                        clickedAnswer = "För tycker de e bra man kan blanda och så lite ris";
-                        createAlertDialog(context);
-                      },
-                    ),
-                    Flexible(
-                        child: Container(
-                            padding: new EdgeInsets.all(20.0),
-                            child: Text(
-                              "Why are some gyms made from wood and some from steel?",
-                              overflow: TextOverflow.clip,
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                        )
-                    ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: (){
-                        //Ta fram svar
-                      },
-                    ),
-                    Flexible(
-                        child: Container(
-                            padding: new EdgeInsets.all(20.0),
-                            child: Text(
-                              "Why is my gym not included in the app?",
-                              overflow: TextOverflow.clip,
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                        )
-                    ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: (){
-                        //Ta fram svar
-                      },
-                    ),
-                    Flexible(
-                        child: Container(
-                            padding: new EdgeInsets.all(20.0),
-                            child: Text(
-                              "How do I give feedback on a gym I recently visited?",
-                              overflow: TextOverflow.clip,
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                        )
-                    ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: (){
-                        //Ta fram svar
-                      },
-                    ),
-                    Flexible(
-                        child: Container(
-                            padding: new EdgeInsets.all(20.0),
-                            child: Text(
-                              "How do I report faulty equipment?",
-                              overflow: TextOverflow.clip,
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                        )
-                    ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: (){
-                        //Ta fram svar
-                      },
-                    ),
-                    Flexible(
-                        child: Container(
-                            padding: new EdgeInsets.all(20.0),
-                            child: Text(
-                              "How do I change my nickname?",
-                              overflow: TextOverflow.clip,
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                        )
-                    ),
-                  ],
-                ),
-              ],
-            ),
+          ExpansionTile(
+            title: Text("Why are some gyms made from wood and some from steel?", style: TextStyle( color: Colors.white),),
+            backgroundColor: Colors.black,
+
+            children: <Widget>[
+              Text("Svar",style: TextStyle( color: Colors.white),),
+
+            ],
           ),
+          ExpansionTile(
+            title: Text("Why is my gym not included in the app?", style: TextStyle( color: Colors.white),),
+            children: <Widget>[
+              Text("Svar",style: TextStyle( color: Colors.white),),
+            ],
+          ),
+          ExpansionTile(
+            title: Text("How do I give feedback on a gym I recently visited?", style: TextStyle( color: Colors.white),),
+            children: <Widget>[
+              Text("Svar",style: TextStyle( color: Colors.white),),
+            ],
+          ),ExpansionTile(
+            title: Text("How do I report faulty equipment?", style: TextStyle( color: Colors.white),),
+            children: <Widget>[
+              Text("Svar",style: TextStyle( color: Colors.white),),
+            ],
+          ),ExpansionTile(
+            title: Text("How do I change my nickname?", style: TextStyle( color: Colors.white),),
+            children: <Widget>[
+              Text("Svar",style: TextStyle( color: Colors.white),),
+            ],
+          ),
+
+
+//          Container(
+//            height: 400.0,
+//            child: ListView(
+//              children: <Widget>[
+//                Row(
+//                  children: <Widget>[
+//                    IconButton(
+//                      icon: Icon(
+//                        Icons.add,
+//                      ),
+//                      onPressed: (){
+//                        clickedFAQ = "Why are some gyms made from wood and some from steel?";
+//                        answer1 = "För tycker de e bra man kan blanda och så lite ris";
+//                        //Create container
+//                        //createAlertDialog(context);
+//                      },
+//                    ),
+//                    Flexible(
+//                        child: Container(
+//                            padding: new EdgeInsets.all(20.0),
+//                            child: Text(
+//                              "Why are some gyms made from wood and some from steel?",
+//                              overflow: TextOverflow.clip,
+//                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+//
+//                            )
+//                        )
+//                    ),
+//
+//                  ],
+//                ),
+//                Row(
+//                  children: <Widget>[
+//                    IconButton(
+//                      icon: Icon(
+//                        Icons.add,
+//                      ),
+//                      onPressed: (){
+//                        //Ta fram svar
+//                      },
+//                    ),
+//                    Flexible(
+//                        child: Container(
+//                            padding: new EdgeInsets.all(20.0),
+//                            child: Text(
+//                              "Why is my gym not included in the app?",
+//                              overflow: TextOverflow.clip,
+//                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+//                            )
+//                        )
+//                    ),
+//                  ],
+//                ),
+//                Row(
+//                  children: <Widget>[
+//                    IconButton(
+//                      icon: Icon(
+//                        Icons.add,
+//                      ),
+//                      onPressed: (){
+//                        //Ta fram svar
+//                      },
+//                    ),
+//                    Flexible(
+//                        child: Container(
+//                            padding: new EdgeInsets.all(20.0),
+//                            child: Text(
+//                              "How do I give feedback on a gym I recently visited?",
+//                              overflow: TextOverflow.clip,
+//                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+//                            )
+//                        )
+//                    ),
+//                  ],
+//                ),
+//                Row(
+//                  children: <Widget>[
+//                    IconButton(
+//                      icon: Icon(
+//                        Icons.add,
+//                      ),
+//                      onPressed: (){
+//                        //Ta fram svar
+//                      },
+//                    ),
+//                    Flexible(
+//                        child: Container(
+//                            padding: new EdgeInsets.all(20.0),
+//                            child: Text(
+//                              "How do I report faulty equipment?",
+//                              overflow: TextOverflow.clip,
+//                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+//                            )
+//                        )
+//                    ),
+//                  ],
+//                ),
+//                Row(
+//                  children: <Widget>[
+//                    IconButton(
+//                      icon: Icon(
+//                        Icons.add,
+//                      ),
+//                      onPressed: (){
+//                        //Ta fram svar
+//                      },
+//                    ),
+//                    Flexible(
+//                        child: Container(
+//                            padding: new EdgeInsets.all(20.0),
+//                            child: Text(
+//                              "How do I change my nickname?",
+//                              overflow: TextOverflow.clip,
+//                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
+//                            )
+//                        )
+//                    ),
+//                  ],
+//                ),
+//              ],
+//            ),
+//          ),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
