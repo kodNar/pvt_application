5566819d5376e6b7fc21976462079b7043eced6c
dirtycoyote <iamclaeskjellberg@gmail.com>
2020-05-14 10:47:18 +0200

    ReportPage with working camera and picture gets saved. FormField now saving the text

--- lib/pages/ReportPage.dart
+++ lib/pages/ReportPage.dart
@@ -22,141 +22,111 @@ import 'package:path_provider/path_provider.dart';
 
 import 'ReportPageEquipmentList.dart';
 
-
 String picPath = 'assets/images/bok.png';
 bool gymChosen = false;
 bool eqChosen = false;
 String description;
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
 Equipment equipment;
 OutdoorGym outdoorGym;
 
-class ReportPage extends StatefulWidget{
+class ReportPage extends StatefulWidget {
   @override
   _ReportPageState createState() => _ReportPageState();
 }
 
-
-
-class _ReportPageState extends State <ReportPage>  {
+class _ReportPageState extends State<ReportPage> {
   final List<OutdoorGym> allOutdoorGym = MapSampleState.allOutdoorGym;
   List<DropdownMenuItem<OutdoorGym>> dropDownMenuItems;
-
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   CameraController _controller;
   Future<void> _initializeControllerFuture;
   bool isCameraReady = false;
   bool showCapturedPhoto = false;
+
 //  bool mounted = false;
   var ImagePath;
 
   @override
-  void initState(){
+  void initState() {}
 
-  }
+  final myController = TextEditingController();
 
   @override
-  /*void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null; //on pause camera is disposed, so we need to call again "issue is only for android"
-    }
-  }*/
-
-
-
-  /*List<DropdownMenuItem<OutdoorGym>> buildDropDownMenuItems(List gyms){
-    List<DropdownMenuItem<OutdoorGym>> items = List();
-    for(OutdoorGym gym in allOutdoorGym){
-      items.add(
-        DropdownMenuItem(
-          value: gym,
-          child: Text(gym.name),
-        ),
-      );
-    }
-    return items;
-  }*/
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 132, 50, 155),
-      appBar: AppBar(title: const Text('Report / Contact'),
-          actions: <Widget>[
-          // action button
-          IconButton(
+      appBar: AppBar(title: const Text('Report / Contact'), actions: <Widget>[
+        // action button
+        IconButton(
           icon: Icon(Icons.home),
-      onPressed: () {
+          onPressed: () {
             picPath = 'assets/images/bok.png';
-        Navigator.pushReplacement(context, MaterialPageRoute(
-          builder: (context) => MapSample()
-        ));
-      },
-    ),
-    ]
-
-      ),
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => MapSample()));
+          },
+        ),
+      ]),
       body: Column(
           key: _formKey,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Container(
-
-                child: gymReturn(),
+              child: gymReturn(),
             ),
             Container(
               child: eqReturn(),
             ),
             Container(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                   ButtonTheme(
-                     shape: RoundedRectangleBorder(
-                       borderRadius: BorderRadius.all(Radius.circular(10)),
-                     ),
-                     buttonColor: Colors.orange,
-                     child: RaisedButton(
-                       child: Row(
-                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                         children: <Widget>[
-                           Text('Take a Picture'),
-                           Icon(Icons.camera_alt),
-                         ],
-                       ),
-                       onPressed: (){
-                         main2(context);
-
-                       },
-                     ),
-                   ),
-
-                    Container(
-
-                      //sök här
-                      width: 200,
-                      height: 200,
-                      child: Image.file(File(picPath)),
-
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ButtonTheme(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  buttonColor: Colors.orange,
+                  child: RaisedButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text('Take a Picture'),
+                        Icon(Icons.camera_alt),
+                      ],
                     ),
-                  ],
-                )
-
+                    onPressed: () {
+                      main2(context);
+                    },
+                  ),
+                ),
+                Container(
+                  //sök här
+                  width: 200,
+                  height: 200,
+                  child: Image.file(File(picPath)),
+                ),
+              ],
+            )),
+            Container(
+              child: Text(
+                'Open Comments',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
             ),
             Container(
-              child: Text('Open Comments', style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-              ),
-            ),
-            Container(
-
-              child: TextFormField(
-
+              child: TextField(
+                controller: myController,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
@@ -170,7 +140,6 @@ class _ReportPageState extends State <ReportPage>  {
                   ),
                   prefixIcon: Icon(Icons.description),
                   hintText: "Describe your issue",
-
                   filled: true,
                   fillColor: Colors.white,
                 ),
@@ -178,62 +147,45 @@ class _ReportPageState extends State <ReportPage>  {
                   color: Colors.black,
                 ),
                 cursorColor: Colors.black38,
-                // ignore: missing_return
-//              validator: (input) {
-//                if (input.isEmpty) {
-//                  return 'Please provide a short description';
-//                }
-//              },
-//                validator: (input) {
-//                  if (input.isEmpty) {
-//                    return 'Please provide a password';
-//                  }
-//                  if (input.length < 6) {
-//                    return 'Your password must be atleast 6 characters';
-//                  }
-//                },
-                onSaved: (input) => description = input,
+
               ),
             ),
-    /*Container(
+            Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 100,
+              child: ButtonTheme(
+                minWidth: 200,
+                height: 50,
+                shape: RoundedRectangleBorder(
 
-              child: */GestureDetector(
-//                onTap: main(),
-      child: Container(
-        alignment: Alignment.center,
-        width: 200,
-        height: 50,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.white,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          gradient: LinearGradient(
-            begin: Alignment.center,
-            colors: <Color>[
-              Color(0xFFF57C00),
-              Color(0xFFFF9800),
-              Color(0xFFFFA726),
-            ],
-          ),
-        ),
-        child: Text('Send',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-            )),
-      ),
-    )
+                  borderRadius: BorderRadius.circular(20.0),
+                  side: BorderSide(color: Colors.white, width: 1.5),
+                ),
 
-
-          ]
-      ),
+                child: RaisedButton(
+//        onPressed: main(),
+                  color: Colors.transparent,
+                  child: Text('Send',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                  ),
+                  onPressed: () {
+                    description = myController.text;
+                    main();
+                  },
+                ),
+              ),
+            )
+          ]),
     );
   }
-  Widget gymReturn(){
-    if(gymChosen){
+
+  Widget gymReturn() {
+    if (gymChosen) {
       return Container(
         padding: EdgeInsets.all(20),
         child: ButtonTheme(
@@ -259,8 +211,8 @@ class _ReportPageState extends State <ReportPage>  {
           ),
         ),
       );
-    }else{
-      return  Container(
+    } else {
+      return Container(
         padding: EdgeInsets.all(20),
         child: ButtonTheme(
           minWidth: 250,
@@ -288,8 +240,8 @@ class _ReportPageState extends State <ReportPage>  {
     }
   }
 
-  Widget eqReturn(){
-    if(eqChosen){
+  Widget eqReturn() {
+    if (eqChosen) {
       return Container(
         padding: EdgeInsets.all(20),
         child: ButtonTheme(
@@ -315,8 +267,8 @@ class _ReportPageState extends State <ReportPage>  {
           ),
         ),
       );
-    }else{
-      return  Container(
+    } else {
+      return Container(
         padding: EdgeInsets.all(20),
         child: ButtonTheme(
           minWidth: 250,
@@ -345,20 +297,24 @@ class _ReportPageState extends State <ReportPage>  {
   }
 
   _pushContextChooseGym(BuildContext context) async {
-    final OutdoorGym result = await Navigator.push(context, MaterialPageRoute(builder: (context) => WorkoutGymList()));
+    final OutdoorGym result = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => WorkoutGymList()));
     gymChosen = true;
     outdoorGym = result;
   }
 
   _pushContextChooseEq(BuildContext context) async {
-    final Equipment result = await Navigator.push(context, MaterialPageRoute(builder: (context) => ReportPageEquipmentList(outdoorGym)));
+    final Equipment result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ReportPageEquipmentList(outdoorGym)));
     eqChosen = true;
     equipment = result;
   }
 
+}
 
 
-  }
 
 
 
@@ -375,13 +331,19 @@ Future<void> main2(context) async {
   // Get a specific camera from the list of available cameras.
   final firstCamera = cameras.first;
 
-  Navigator.push(context, MaterialPageRoute(builder: (context) => TakePictureScreen(camera: firstCamera) )); /// Sätt din testsida här! ///
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => TakePictureScreen(camera: firstCamera)));
+
+  /// Sätt din testsida här! ///
   //TakePictureScreen(camera: firstCamera);
 }
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
+
   const TakePictureScreen({
     Key key,
     @required this.camera,
@@ -463,7 +425,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 
             print(picPath.toString());
 
-
             // If the picture was taken, display it on a new screen.
             /*Navigator.push(
               context,
@@ -471,11 +432,8 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                 builder: (context) => DisplayPictureScreen(imagePath: path),
               ),
             );*/
-            Navigator.pushReplacement(context, MaterialPageRoute(
-              builder: (context) => ReportPage()
-            )
-            );
-
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => ReportPage()));
           } catch (e) {
             // If an error occurs, log the error to the console.
             print(e);
@@ -486,9 +444,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   }
 }
 
-
 ///////////////////////////////////EMAIL///////////////////////////////////////////
-
 
 main() async {
 //  final formState = _formKey.currentState;
@@ -497,29 +453,30 @@ main() async {
 //    formState.save(); //ser till att vi kan hämta variablerna.
 //  }
 
-    gymChosen = false;
-    eqChosen = false;
-    String userName = 'apikey';
-    String passWord = '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';
-    final smtpServer = SmtpServer(
-        'smtp.sendgrid.net', username: userName, password: passWord);
+  gymChosen = false;
+  eqChosen = false;
+  String userName = 'apikey';
+  String passWord =
+      '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';
+  final smtpServer =
+      SmtpServer('smtp.sendgrid.net', username: userName, password: passWord);
 
-    final message = Message()
-      ..from = Address('simon.schoolsoft@gmail.com', 'Simon')
-      ..recipients.add('simon.schoolsoft@gmail.com')
-      ..subject = 'Test Dart Mailer library ${DateTime.now()}'
-      ..text = 'haj';/*('Gym: ' + outdoorGym.name + '\n' + 'Equipment: ' +
+  final message = Message()
+    ..from = Address('simon.schoolsoft@gmail.com', 'Simon')
+    ..recipients.add('simon.schoolsoft@gmail.com')
+    ..subject = 'Test Dart Mailer library ${DateTime.now()}'
+    ..text = description;
+  /*('Gym: ' + outdoorGym.name + '\n' + 'Equipment: ' +
           equipment.getName() + '\n' + description)*/
 //      ..attachments = Image.file(File(picPath)) as List<Attachment>;
 
-
-    try {
-      final sendReport = await send(message, smtpServer);
-      print('Message sent: ' + sendReport.toString());
-    } on MailerException catch (e) {
-      print('Message not sent.');
-      for (var p in e.problems) {
-        print('Problem: ${p.code}: ${p.msg}');
-      }
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('Message sent: ' + sendReport.toString());
+  } on MailerException catch (e) {
+    print('Message not sent.');
+    for (var p in e.problems) {
+      print('Problem: ${p.code}: ${p.msg}');
     }
   }
+}
