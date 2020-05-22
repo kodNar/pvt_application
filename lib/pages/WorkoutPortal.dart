a15631673fff9c0b56950260e4b5061106ef4836
kodNar <56548619+kodNar@users.noreply.github.com>
2020-05-22 11:46:45 +0200

    Färger på WorkoutPortal och Homepage

--- lib/pages/WorkoutPortal.dart
+++ lib/pages/WorkoutPortal.dart
@@ -5,6 +5,7 @@ import 'package:flutterapp/pages/MapsTest.dart';
 import 'package:flutterapp/pages/RecentWorkout.dart';
 import 'package:flutterapp/pages/WorkoutLog.dart';
 import 'package:flutterapp/widgets/Appbar.dart';
+
 class WorkoutPortal extends StatefulWidget {
   @override
   _WorkoutPortalState createState() => _WorkoutPortalState();
@@ -14,87 +15,106 @@ class _WorkoutPortalState extends State<WorkoutPortal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xFF84329b),
         appBar: BaseAppBar(
           title: 'Workouts',
         ),
         body: Center(
           child: Container(
-            padding: EdgeInsets.all(40),
-            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ExistingWorkouts()));
-                },
-                child: Container(
-                  width: 350,
-                  height: 135,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2,
-                    ),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                      colors: <Color>[
-
-                        Color(0xFF84329b),
-                        Color(0xFF84329b),
-                        Color(0xFF9438ae),
-                        Color(0xFFa53fc1),
-
-                     //   Color(0xFFB388FF),
-                      ],
+            padding: EdgeInsets.all(15),
+            child: Column(mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Currently signed as:',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                     ),
                   ),
-                  child: Text('+ Log new workout',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 30,
-                      )),
-                ),
-              ),
-              SizedBox(height: 30),
-              InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RecentWorkouts()));
-                },
-                child: Container(
-                  width: 350,
-                  height: 135,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.5,
+                  SizedBox(height: 30),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) => ExistingWorkouts()));
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: <Color>[
+
+                            Color(0xFF84329b),
+                            Color(0xFF9438ae),
+                            Color(0xFFa53fc1),
+                            Color(0xFFae52c7),
+
+                            //   Color(0xFFB388FF),
+                          ],
+                        ),
+                      ),
+                      child: Text('+ Log new workout',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 30,
+                          )),
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    gradient: LinearGradient(
+                  ),
+                  SizedBox(height: 30),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) => RecentWorkouts()));
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.5,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        gradient: LinearGradient(
 
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
 
-                      colors: <Color>[
-                        Color(0xFF6200EA),
-                        Color(0xFF651FFF),
-                        Color(0xFF7C4DFF),
-                        Color(0xFFB388FF),
+                          colors: <Color>[
+                            Color(0xFF84329b),
+                            Color(0xFF84329b),
+                            Color(0xFF9438ae),
+                            Color(0xFFa53fc1),
+                            Color(0xFFae52c7),
+                            /*
+                            SNYGGA men fel tema..?
+                            Color(0xFF6200EA),
+                            Color(0xFF651FFF),
+                            Color(0xFF7C4DFF),
+                            Color(0xFFB388FF),
 
-                        /*
+                             */
+
+                            /*
                         Color(0xFF5E35B1),
                         Color(0xFF673AB7),
                         Color(0xFF7E57C2),
                         Color(0xFF9575CD),
 
                          */
-                        /*
+                            /*
                         Color(0xFF6A1B9A),
                         Color(0xFF7B1FA2),
                         Color(0xFF8E24AA),
@@ -103,53 +123,62 @@ class _WorkoutPortalState extends State<WorkoutPortal> {
                         Color(0xFFBA68C8),
                         */
 
-                      ],
+                          ],
+                        ),
+                      ),
+                      child: Text('Discover workouts',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 30,
+                          )),
                     ),
                   ),
-                  child: Text('Discover workouts',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 30,
-                      )),
-                ),
-              ),
-              SizedBox(height: 30),
-              InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RecentWorkouts()));
-                },
-                child: Container(
-                  width: 350,
-                  height: 135,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.5,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
+                  SizedBox(height: 30),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) => RecentWorkouts()));
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.5,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
 
-                      colors: <Color>[
+                          colors: <Color>[
+                            Color(0xFF84329b),
+                            Color(0xFF84329b),
+                            Color(0xFF9438ae),
+                            Color(0xFFa53fc1),
+                            Color(0xFFae52c7),
 
-                        Color(0xFF6200EA),
-                        Color(0xFF651FFF),
-                        Color(0xFF7C4DFF),
-                        Color(0xFFB388FF),
+                            /*
+                            Color(0xFF6200EA),
+                            Color(0xFF651FFF),
+                            Color(0xFF7C4DFF),
+                            Color(0xFFB388FF),
 
-                        /*
+                             */
+
+                            /*
                         Color(0xFF5E35B1),
                         Color(0xFF673AB7),
                         Color(0xFF7E57C2),
                         Color(0xFF9575CD),
 
                          */
-                        /*
+                            /*
                         Color(0xFF6A1B9A),
                         Color(0xFF7B1FA2),
                         Color(0xFF8E24AA),
@@ -158,19 +187,19 @@ class _WorkoutPortalState extends State<WorkoutPortal> {
                         Color(0xFFBA68C8),
                         */
 
-                      ],
+                          ],
+                        ),
+                      ),
+                      child: Text('My workouts',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 30,
+                          )),
                     ),
                   ),
-                  child: Text('My workouts',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 30,
-                      )),
-                ),
-              ),
-            ]),
+                ]),
           ),
         ));
   }
